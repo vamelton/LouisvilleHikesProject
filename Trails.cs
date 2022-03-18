06d95fb18955cfe5b731027c80fe89b7de3fe568
Valerie Melton <vwimple@gmail.com>
2022-03-18 19:58:35 -0400

    Updated Initislize

--- Trails.cs
+++ Trails.cs
@@ -8,23 +8,23 @@ using System.Threading.Tasks;
 
 namespace LouisvilleHikes
 {
-    public class Trails
+    public class Trail
     {
         private string park;
         private string trail;
         private double distance;
         private string difficulty;
         private string name;
-        private string timespan;
+      
 
-        public Trails()
+        public Trail()
         {
         }
 
-        public Trails(string v) => V = v;
+        public Trail(string v) => V = v;
 
         public string Park { get => park; set => park = value; }
-        public string Trail { get => trail; set => trail = value; }
+        public string Name { get => trail; set => trail = value; }
         public double Distance { get => distance; set => distance = value; }
         public string Difficulty { get => difficulty; set => difficulty = value; }
         public string V { get; }
@@ -35,55 +35,58 @@ namespace LouisvilleHikes
 
 }
 
-public List<Trails>
-public Trails() => LazyInitializer();
-void LazyInitializer()
+public class Trails : List<Trail>
 {
-    throw new NotImplementedException();
+    public Trails()
+    {
+        Initialize();
+    }
+    private void Initialize()
+    {
+        Trail trail1 = new Trail("Red Trail");
+        trail1.Park = "Jefferson Memorial Forest";
+        trail1.Distance = (4.8);
+        trail1.Difficulty = "Moderate";
+        trail1.TimeSpan = new TimeSpan(2, 35, 0);
+
+
+        Trail trail2 = new Trail("Silstone Trail");
+        trail2.Park = "Jefferson Memorial Forest";
+        trail2.Distance = (6.7);
+        trail2.Difficulty = "Strenuous";
+        trail2.TimeSpan = new TimeSpan(3, 40, 0);
+
+        Trail trail3 = new Trail("Mitchell Hill Lake Loop");
+        trail3.Park = "Jefferson Memorial Forest";
+        trail3.Distance = (1.4);
+        trail3.Difficulty = "Easy";
+        trail3.TimeSpan = new TimeSpan(1, 30, 0);
+
+        Trail trail4 = new Trail("Fresh Air Loop");
+        trail4.Park = "Waverly Park";
+        trail4.Distance = (3);
+        trail4.Difficulty = "Strenuous";
+        trail4.TimeSpan = new TimeSpan(1, 30, 0);
+
+        Trail trail5 = new Trail("Playground Loop");
+        trail5.Park = "Waverly Park";
+        trail5.Distance = (0.6);
+        trail5.Difficulty = "Easy";
+        trail5.TimeSpan = new TimeSpan(0, 18, 0);
+
+        Trail trail6 = new Trail("Twisting Bends Loop");
+        trail6.Park = "Waverly Park";
+        trail6.Distance = (2.2);
+        trail6.Difficulty = "Moderate";
+        trail6.TimeSpan = new TimeSpan(1, 6, 0);
+
+        List<Trail> Trails = new List<Trail>();
+        Add(trail1);
+        Add(trail2);
+        Add(trail3);
+        Add(trail4);
+        Add(trail5);
+        Add(trail6);
+    }
 }
-{
-    Trails trail1 = new Trails("Red Trail");
-    trail1.Park = "Jefferson Memorial Forest";
-    trail1.Distance = (4.8);
-    trail1.Difficulty = "Moderate";
-    trail1.TimeSpan = new TimeSpan(2, 35, 0);
 
-
-    Trails trail2 = new Trails("Silstone Trail");
-    trail2.Park = "Jefferson Memorial Forest";
-    trail2.Distance = (6.7);
-    trail2.Difficulty = "Strenuous";
-    trail2.TimeSpan = new TimeSpan(3, 40, 0);
-
-    Trails trail3 = new Trails("Mitchell Hill Lake Loop");
-    trail3.Park = "Jefferson Memorial Forest";
-    trail3.Distance = (1.4);
-    trail3.Difficulty = "Easy";
-    trail3.TimeSpan = new TimeSpan(1, 30, 0);
-
-    Trails trail4 = new Trails("Fresh Air Loop");
-    trail4.Park = "Waverly Park";
-    trail4.Distance = (3);
-    trail4.Difficulty = "Strenuous";
-    trail4.TimeSpan = new TimeSpan(1, 30, 0);
-
-    Trails trail5 = new Trails("Playground Loop");
-    trail5.Park = "Waverly Park";
-    trail5.Distance = (0.6);
-    trail5.Difficulty = "Easy";
-    trail5.TimeSpan = new TimeSpan(0, 18, 0);
-
-    Trails trail6 = new Trails("Twisting Bends Loop");
-    trail6.Park = "Waverly Park";
-    trail6.Distance = (2.2);
-    trail6.Difficulty = "Moderate";
-    trail6.TimeSpan = new TimeSpan(1, 6, 0);
-
-    List<Trails> Trails = new List<Trails>();
-    Trails.Add(trail1);
-    Trails.Add(trail2);
-    Trails.Add(trail3);
-    Trails.Add(trail4);
-    Trails.Add(trail5);
-    Trails.Add(trail6);
-}
